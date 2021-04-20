34cd015d34b60c653f06a43608c6dc5ca4c3fb75
Tran Van <vanbaotran201194@gmail.com>
2021-04-20 18:12:01 +0200

    base done

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require ('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req,res,next) => res.sendFile(__dirname+'/views/home.html'))
+app.get('/about',(req,res,next) => res.sendFile(__dirname+'/views/about.html'))
+app.get('/works',(req,res,next) => res.sendFile(__dirname+'/views/works.html'))
+app.listen(3000, ()=>console.log('running on port 3000'))
